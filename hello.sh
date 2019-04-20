422ffcbbc6e32d7d68f8461bce1e160b179ac83a
John Vargas <john062169@gmail.com>
2019-04-20 01:12:24 -0500

    Added extra functionality

--- hello.sh
+++ hello.sh
@@ -12,34 +12,45 @@ function kernel_version()
 }
 function system_status()
 {
-	echo $(systemctl status firewalld)
+	systemctl status firewalld
 }
 function script_temp()
 {
+	# User input for script 
 	printf "\nName of script: "
 	read script_n
 	printf "\nScript destination: "
 	read dest
-	
 	script_path=$dest$script_n
 	
 	printf "\nScript preview: $script_path \nCreate this script?[y/n]"
 	read ans
+	# Check if user accepts new script
 	if [ $ans == 'y'  ]
 	then
 		touch $script_path
 		chmod u+x $script_path
+		echo '#!/bin/bash' >> $script_path
 		printf "\nScript '$script_path' has been created."
 	else
 		printf "\nScript discarded"
 	fi
 }
+function backup_config_files()
+{
+	cp /etc/*.conf /backup
+	printf "Backup complete\n"
+}
+
+## Main code body ###
+#####################
 
 printf "Hello "$name", what would you like to do?"
 
 while [ $input != 'q'  ];
 do
-	printf "\n1.) List kernel version\n2.) Show firewall status\n3.) Create script template"
+	printf "\n-----------------------------------\nSelect from the following -----------\n1.) List kernel version\n2.) Show firewall status\n3.) Create script template\n4.) Backup config files\n"
+	
 	printf "Type: 'h' for help and 'q' to quit\n"
 	
 	#read -p '' input
@@ -55,6 +66,9 @@ do
 	elif [ $input == 3 ]
 	then	
 		script_temp
+	elif [ $input == 4  ]
+	then
+		backup_config_files
 	else
 		printf "Invalid selection, try again\n"
 	fi
